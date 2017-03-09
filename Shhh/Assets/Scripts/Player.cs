be289af20c6fee5d39d8c80b3cce1be5129bf333
Ricardo <ricardo.g.pereira@ist.utl.pt>
2017-03-09 15:56:06 +0000

    just a cube moving around

---
+++ Shhh/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour {
+
+    public float movSpeed = 2.0f;
+    public float runSpeed = 5.0f;
+
+    PlayerController controller;
+
+    private bool running = false;
+
+    void Start () {
+        controller = GetComponent<PlayerController>();
+    }
+
+    private void checkRunning()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            running = true;
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            running = false;
+        }
+    }
+
+    void Update () {
+        Vector3 mov = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = mov.normalized;
+
+        checkRunning();
+
+        moveVelocity *= running ? runSpeed : movSpeed;
+
+        controller.Move(moveVelocity);
+    }
+}
---
+++ Shhh/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Vector3 velocity;
+    Rigidbody myRigidbody;
+
+    void Start () {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
