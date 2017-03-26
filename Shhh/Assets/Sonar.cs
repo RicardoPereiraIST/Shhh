018e465017224076d0cb58b79e31a34de5942a50
Marcelo Silva <m.m.silva@accenture.com>
2017-03-26 17:27:18 +0100

    Just a stupid commit

---
+++ Shhh/Assets/Sonar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sonar : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("enemy"))
+        {
+            enemy x = other.gameObject.GetComponent<enemy>();
+            x.inRange = true;
+            x.lastX = gameObject.transform.position.x;
+            x.lastZ = gameObject.transform.position.z;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag.Equals("enemy"))
+        {
+            enemy x = other.gameObject.GetComponent<enemy>();
+            x.inRange = true;
+            x.lastX = gameObject.transform.position.x;
+            x.lastZ = gameObject.transform.position.z;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("enemy"))
+        {
+            enemy x = other.gameObject.GetComponent<enemy>();
+            x.inRange = false;
+            x.lastX = gameObject.transform.position.x;
+            x.lastZ = gameObject.transform.position.z;
+        }
+    }
+}
---
+++ Shhh/Assets/enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+
+    public bool inRange;
+    public float lastX;
+    public float lastZ;
+
+    // Use this for initialization
+    void Start()
+    {
+        inRange = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRange)
+        {
+            float step = 0.85f * Time.deltaTime;
+            Vector3 position = new Vector3(lastX, 0, lastZ);
+            transform.position = Vector3.MoveTowards(transform.position, position, step);
+        }
+    }
+}
