23a25fd1a9b1015b54b3f260199803515aeaca4a
Marcelo Silva <m.m.silva@accenture.com>
2017-04-26 22:28:59 +0100

    The starting of abilities.

---
+++ Shhh/Assets/ScriptableObjects/AbilitiesScripts/Distraction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Abilities/Distraction")]
+public class DistractionAbility : Ability {
+
+    public int gunDamage = 1;
+    public float weaponRange = 50f;
+    public float hitforce = 100f;
+
+
+    public override void Initialize(GameObject obj)
+    {
+        return;
+    }
+
+    public override void TriggerAbility()
+    {
+        return;
+    }
+}
