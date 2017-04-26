245b13abefbb033b829250b173783ae0f2aef3ae
Marcelo Silva <m.m.silva@accenture.com>
2017-04-26 11:44:44 +0100

    Some more changes

---
+++ Shhh/Assets/DistractionScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class DistractionScript : Entity
+{
+    // Use this for initialization
+    void Start()
+    {
+        movSpeed = 10f;
+        runSpeed = 10f;
+        showLight = true;
+        isMoving = true;
+    }
+
+    // Update is called once per frame
+    new void Update()
+    {
+        base.Update();
+    }
+}
