775f6358e0aa61d8bf2cccdb22579f7fdd8d9207
Marcelo Silva <m.m.silva@accenture.com>
2017-05-01 23:00:30 +0100

    Some fixes and controls

--- Shhh/Assets/Scripts/Menu/StartMenu.cs
+++ Shhh/Assets/Scripts/Menu/StartMenu.cs
@@ -3,8 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class StartMenu : MonoBehaviour {
+
+    public GameObject img;
+    public GameObject workaround;
+    private bool isControlOn;
+
+    void Start()
+    {
+        //Only for prototype, can't fix redraw
+        img.SetActive(false);
+    }
+
+    void Update()
+    {
+        workaround.SetActive(false);
+        if (Input.GetButton("Fire1") && img.activeSelf)
+        {
+            //Only for prototype, can't fix redraw
+            img.SetActive(false);
+            workaround.SetActive(true);
+        }
+    }
 
     public void startGame()
     {
@@ -15,5 +37,11 @@ public class StartMenu : MonoBehaviour {
 	public void exitGame()
     {
         Application.Quit();
+    }
+
+    public void controls()
+    {
+        //Only for prototype, can't fix redraw
+        img.SetActive(true);
     }
 }
