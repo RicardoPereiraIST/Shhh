d7528b44948f15e1ee89ff61b04d306e748e5a35
Ricardo <ricardo.g.pereira@ist.utl.pt>
2017-05-01 19:39:30 +0100

    simple main menu and gameover added

---
+++ Shhh/Assets/Scripts/Menu/GameOver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+
+    public Canvas over;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == FindObjectOfType<Player>().tag)
+        {
+            Time.timeScale = 0;
+            GameObject ui = GameObject.Find("UI/");
+            ui.SetActive(false);
+
+            over.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Shhh/Assets/Scripts/Menu/StartMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+
+    public void startGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("prototype_level_test");
+    }
+
+	public void exitGame()
+    {
+        Application.Quit();
+    }
+}
