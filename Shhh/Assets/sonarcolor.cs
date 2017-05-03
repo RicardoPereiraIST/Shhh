2e56a02f210ef57fdb32cc4e9c5c40a5d91ae570
Marcelo Silva <m.m.silva@accenture.com>
2017-05-03 20:18:21 +0100

    Some colors

---
+++ Shhh/Assets/sonarcolor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sonarcolor : MonoBehaviour
+{
+    public Color edgeColor;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<Renderer>().material.SetColor("_EdgeColor", edgeColor);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag.Equals("enemy"))
+        {
+            enemy x = other.gameObject.GetComponent<enemy>();
+            x.lastX = gameObject.transform.position.x;
+            x.lastZ = gameObject.transform.position.z;
+        }
+        other.GetComponent<Renderer>().material.SetColor("_EdgeColor", edgeColor);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.tag.Equals("enemy"))
+        {
+            enemy x = other.gameObject.GetComponent<enemy>();
+            x.lastX = gameObject.transform.position.x;
+            x.lastZ = gameObject.transform.position.z;
+        }
+        other.GetComponent<Renderer>().material.SetColor("_EdgeColor", edgeColor);
+    }
+}
